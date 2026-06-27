1b49c6bf503d2e3b5f21355ab00a7fe5ecae4fb3
sauravphuyal01 <sauravphwork@gmail.com>
2026-06-27 21:02:40 +0545

    Bug fix 1

--- Frontend/src/layouts/MainLayout.jsx
+++ Frontend/src/layouts/MainLayout.jsx
@@ -1,9 +1,11 @@
 import Navbar from '../components/Navbar/Navbar';
 import Footer from '../components/Footer/Footer';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import './MainLayout.css';
 
 function MainLayout() {
+  const location = useLocation();
+
   return (
     <div className="main-layout">
       <Navbar />
@@ -12,7 +14,7 @@ function MainLayout() {
         <Outlet />
       </main>
       
-      <Footer />
+      {location.pathname === '/' && <Footer />}
     </div>
   );
 }
